8dc0d748d4ca7c74dad71b7dbe2db18f8bf12af5
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-04 16:08:38 +0900

    settings: 各種設定ファイルを2.4.0系に合わせた(#61)

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,19 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
  */
 
-module.exports = {
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
   dummy: ['dummy'],
 };
+
+module.exports = sidebars;
